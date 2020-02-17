b5787e0c34cc82db673f9d2b97ec42aad0cd4420
1used23 <diziwii@gmail.com>
2020-02-18 00:52:36 +0300

    Done states and output of state elements

--- src/App.js
+++ src/App.js
@@ -4,13 +4,44 @@ import { Button, TextField } from "@material-ui/core";
 import "./App.css";
 
 function App() {
+  const [tasksState, setState] = useState([]);
+  const [textFieldState, setTextFieldState] = useState(0);
+
+  const handleButton = () => {
+    if (textFieldState) {
+      setState([
+        ...tasksState,
+        {
+          task: textFieldState
+        }
+      ]);
+      document.querySelector("#textfield").value = "";
+    }
+  };
+
+  const handleInput = e => {
+    const textInput = e.target.value;
+    setTextFieldState(textInput);
+  };
+
   return (
     <div className="App">
+      <div className="title">Simple ToDo List!</div>
       <div className="form">
-        <TextField id="multil" label="Put some text in here!" multiline />
-        <Button variant="contained" color="primary">
+        <TextField
+          id="textfield"
+          label="Put some text in here!"
+          multiline
+          onChange={handleInput}
+        />
+        <Button variant="contained" color="primary" onClick={handleButton}>
           ADD
         </Button>
+      </div>
+      <div className="main-list">
+        {tasksState.map(task => {
+          return <ul>{task.task}</ul>;
+        })}
       </div>
     </div>
   );
