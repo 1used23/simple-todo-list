4968b081bf6a2cdcab692ddecc371ce268ca2607
1used23 <diziwii@gmail.com>
2020-02-17 23:38:30 +0300

    Added form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="form">
+        <TextField id="multil" label="Put some text in here!" multiline />
+        <Button variant="contained" color="primary">
+          ADD
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
