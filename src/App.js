ec52592db8bce28d26e4616f2d38b19b471193eb
1used23 <diziwii@gmail.com>
2020-02-18 19:56:31 +0300

    Now we can delete element

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { useState } from "react";
-import { Button, TextField } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  List,
+  ListItem,
+  ListItemText,
+  IconButton,
+  ListItemSecondaryAction,
+  Container
+} from "@material-ui/core";
 import { CSSTransitionGroup } from "react-transition-group";
+import Typography from "@material-ui/core/Typography";
+
+import DeleteIcon from "@material-ui/icons/Delete";
+
 import "./App.css";
 
 function App() {
   const [tasksState, setState] = useState([]);
   const [textFieldState, setTextFieldState] = useState(0);
 
-  const handleButton = () => {
+  const handleAddButton = () => {
     if (textFieldState) {
-      setState([
-        ...tasksState,
-        {
-          task: textFieldState
-        }
-      ]);
+      const id = tasksState.length;
+      setState([...tasksState, { id: id, task: textFieldState }]);
       document.querySelector("#textfield").value = "";
     }
+  };
+
+  const handleDeleteButton = e => {
+    const id = e.target.dataset.id;
+    console.log(id);
   };
 
   const handleInput = e => {
@@ -26,30 +40,44 @@ function App() {
 
   return (
     <div className="App">
-      <div className="title">Simple ToDo List!</div>
-      <div className="form">
-        <TextField
-          id="textfield"
-          label="Put some text in here!"
-          multiline
-          onChange={handleInput}
-        />
-        <Button variant="contained" color="primary" onClick={handleButton}>
-          ADD
-        </Button>
-      </div>
+      <Container maxWidth="xs">
+        <Typography variant="h4">Simple ToDo List!</Typography>
+        <div className="form">
+          <TextField
+            id="textfield"
+            label="Put some text in here!"
+            multiline
+            onChange={handleInput}
+          />
+          <Button variant="contained" color="primary" onClick={handleAddButton}>
+            ADD
+          </Button>
+        </div>
 
-      <div className="main-list">
-        <CSSTransitionGroup
-          transitionName="example"
-          transitionEnterTimeout={500}
-          transitionLeaveTimeout={300}
-        >
-          {tasksState.map(task => {
-            return <ul key={task.task}>{task.task}</ul>;
-          })}
-        </CSSTransitionGroup>
-      </div>
+        <List>
+          <CSSTransitionGroup
+            transitionName="example"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={300}
+          >
+            {tasksState.map(task => {
+              return (
+                <ListItem key={task.task}>
+                  <ListItemText primary={task.task} />
+                  <IconButton
+                    edge="end"
+                    aria-label="delete"
+                    data-id={task.id}
+                    onClick={handleDeleteButton}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItem>
+              );
+            })}
+          </CSSTransitionGroup>
+        </List>
+      </Container>
     </div>
   );
 }
