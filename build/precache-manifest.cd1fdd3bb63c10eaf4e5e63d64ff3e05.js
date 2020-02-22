d17ce0196fcfc04a6faa8e59927c2ebe29d42d7a
1used23 <diziwii@gmail.com>
2020-02-23 02:21:50 +0300

    build

---
+++ build/precache-manifest.cd1fdd3bb63c10eaf4e5e63d64ff3e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67fd4aa48f2e071c4895ce825da17db9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49ff182fbc3746d87ea2",
+    "url": "/static/css/main.afff25cf.chunk.css"
+  },
+  {
+    "revision": "b6756cd3f5afdd25ccd5",
+    "url": "/static/js/2.80644103.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.80644103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ff182fbc3746d87ea2",
+    "url": "/static/js/main.bd492775.chunk.js"
+  },
+  {
+    "revision": "74cf8dcb019b3c08c6d0",
+    "url": "/static/js/runtime-main.f4a72f7c.js"
+  }
+]);
