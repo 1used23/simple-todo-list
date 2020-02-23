07c86558300a030dd6b94415ad0b7daeec45c4a7
1used23 <diziwii@gmail.com>
2020-02-24 02:54:54 +0300

    gh-pages added

---
+++ build/precache-manifest.bfcc0f3d6bada2059e3ef3d9e5e43058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4468c9df540d36fb7992def4d3fded49",
+    "url": "/simple-todo-list/index.html"
+  },
+  {
+    "revision": "a12112889f4a532761be",
+    "url": "/simple-todo-list/static/css/main.01e21cd9.chunk.css"
+  },
+  {
+    "revision": "b6756cd3f5afdd25ccd5",
+    "url": "/simple-todo-list/static/js/2.80644103.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/simple-todo-list/static/js/2.80644103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12112889f4a532761be",
+    "url": "/simple-todo-list/static/js/main.a9a01eb6.chunk.js"
+  },
+  {
+    "revision": "558b6c2157a164582992",
+    "url": "/simple-todo-list/static/js/runtime-main.921a2655.js"
+  }
+]);
